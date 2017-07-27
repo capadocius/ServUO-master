492d8ccafbcecdd227b1b371bab8724bef651b88
capadocius <mitch603@hotmail.com>
2017-07-27 16:39:35 -0400

    Zystria
    systèmeXP

---
+++ Scripts/Evolution/XpSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server.Evolution
+{
+    public class XpSystem
+    {
+        [CommandProperty(AccessLevel.GameMaster)]
+        public int pointXP { get; set; }
+
+        public void Serialize(GenericWriter writer)
+        {
+            writer.Write(36);  //version
+            writer.Write(pointXP);
+        }
+
+        public XpSystem()
+        {
+            pointXP = 0;
+        }
+
+        public XpSystem(GenericReader reader)
+        {
+            int version = reader.ReadInt();
+            pointXP = reader.ReadInt();
+        }
+    }
+}
